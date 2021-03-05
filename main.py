2046b01f66d20f5696434aa96739c0260514938e
Hari <hari@shankar.com>
2021-03-05 12:10:38 +0530

    @step537

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DecimalField
 from wtforms.validators import DataRequired
 import requests
 from typing import Callable
@@ -14,6 +14,12 @@ Bootstrap(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///day64.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class EditForm(FlaskForm):
+    rating = DecimalField(label = "Your Rating Out of 10", validators = [DataRequired()])
+    review = StringField(label = "Your Review", validators = [DataRequired()])
+    submit = SubmitField(label = "Save Changes")
+
 
 class MySQLAlchemy(SQLAlchemy):
     Column: Callable
@@ -53,6 +59,18 @@ db.create_all()
 def home():
     data = db.session.query(Movie).all()
     return render_template("index.html", movies = data)
+
+@app.route("/edit/<mov_id>", methods = ['GET','POST'])
+def edit(mov_id):
+    editform = EditForm()
+    movie = Movie.query.get(mov_id)
+    if editform.validate_on_submit():
+        movie.rating = editform.rating.data
+        movie.review = editform.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", form = editform, movie = movie)
+    
 
 
 if __name__ == '__main__':
